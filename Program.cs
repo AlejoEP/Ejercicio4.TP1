d022fb922a16db77c103b1f3c6c73bc78f8c158d
Alejo <alejoepucheta89@gmail.com>
2022-09-21 20:53:18 -0300

    Ingresar 2 palabras y determinar si es palindromo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Palindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una palabra:");
+            string palabra = Console.ReadLine();
+
+            Console.WriteLine("Ingrese otra palabra:");
+            string palabra2 = Console.ReadLine();
+
+            string palabranueva = "";
+            for (int i = palabra.Length -1; i>=0; i--)
+            {
+                palabranueva = palabranueva + palabra[i];
+            }
+            if (palabra == palabra2)
+            {
+                Console.WriteLine("Son palindromos");
+                
+            }
+            else
+            {
+                Console.WriteLine("No son palindromos");
+            }
+        }
+    }
+}
